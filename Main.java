38dca35dba45245fe3d11bf68aebe3aca4a7bf55
iermina <167538000+iermina@users.noreply.github.com>
2024-09-25 23:04:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,25 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder b = new Feeder(c:0);
-        System.out.print(b.simulateManyDays(numBirds:5, numDays:10));
+        Feeder x = new Feeder(500);
+        x.simulateOneDay(12);
+        System.out.println(x);
+        Feeder y = new Feeder(1000);
+        y.simulateOneDay(22);
+        System.out.println(y);
+        Feeder z = new Feeder(100);
+        z.simulateOneDay(5);
+        System.out.println(z);
+
+        Feeder a = new Feeder(0);
+        System.out.println(a.simulateManyDays(5,10));
+        Feeder b = new Feeder(250);
+        System.out.println(b.simulateManyDays(10,5));
+        Feeder c = new Feeder(2400);
+        System.out.println(c.simulateManyDays(10,4));
+        
+
+        
+
     }
 }
-System.out.println(b);
-System.out.println(b.currentFood);
